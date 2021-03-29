315e9e6d93d4c37777d0fc28781731012ed648f8
Garrett Sloup <garrettsloup@gmail.com>
2021-03-29 15:15:03 -0500

    Makes basic ui and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GoalsComponent } from './goals/goals.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'goals', component: GoalsComponent},
+  {path: '**', redirectTo:"/goals"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
